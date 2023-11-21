6bf945b017d5709c6ac355a1b2685e034b03d61a
Laura Ophélie <lauraophelie1@gmail.com>
2023-11-21 12:01:49 +0100

    composants

---
+++ src/Liste.jsx
@@ -0,0 +1,8 @@
+function Liste() {
+    return (
+        <>
+        </>
+    )
+}
+
+export default Liste;
---
+++ src/Resultat.jsx
@@ -0,0 +1,9 @@
+function Resultat() {
+    return (
+        <div className="resultat">
+
+        </div>
+    )
+}
+
+export default Resultat;
